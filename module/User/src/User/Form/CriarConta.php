7ed2f34206860c6ca6ded6da5eeb63bedded9346
Marcelo Ogeda <marcelo.ogeda@gmail.com>
2013-10-07 00:01:47 -0300

    Updated Form CriarConta and created CriarContaFilter

---
+++ module/User/src/User/Form/CriarConta.php
@@ -0,0 +1,264 @@
+<?php
+
+namespace User\Form;
+
+use Zend\Form\Form;
+
+
+class CriarConta extends Form
+{
+    protected $tipoUsuario;
+    protected $estados;
+
+
+	function __construct($arrTipoUsuario = array(), $arrEstados = array())
+	{
+		parent::__construct('form-create-account', array(), array());
+
+        $this->tipoUsuario = $arrTipoUsuario;
+        $this->estados     = $arrEstados;
+
+		// Tipo Conta
+        $this->add(array(
+            'name' => 'tipo_conta',
+            'type' => 'Zend\Form\Element\Select',
+            'options' => array(
+                'label' => 'Tipo de conta',
+                'options' => array(
+                	0 => 'Pessoa Física',
+                	1 => 'Pessoa Juridica',
+                ),
+            ),
+            'attributes' => array(
+                'class' => 'custom-select span4 no-margin mleft',
+            )
+        ));
+
+
+        // Nome Completo
+        $this->add(array(
+            'name' => 'nome',
+            'type' => 'Zend\Form\Element\Text',
+            'options' => array(
+                'label' => 'Nome Completo',
+            ),
+            'attributes' => array(
+                'class' => 'span8',
+            )
+        ));
+
+
+        // Data Nascimento
+        $this->add(array(
+            'name' => 'data_nasc',
+            'type' => 'Zend\Form\Element\Text',
+            'options' => array(
+                'label' => 'Data Nascimento',
+                'options' => $this->getOptionsForSelect($this->tipoUsuario, 'tipo')
+            ),
+            'attributes' => array(
+                'class' => 'span2',
+            )
+        ));
+
+
+        // E-mail
+        $this->add(array(
+            'name' => 'email',
+            'type' => 'Zend\Form\Element\Text',
+            'options' => array(
+                'label' => 'E-mail',
+            ),
+            'attributes' => array(
+                'class' => 'span6',
+            )
+        ));
+
+
+        // Telefone
+        $this->add(array(
+            'name' => 'telefone',
+            'type' => 'Zend\Form\Element\Text',
+            'options' => array(
+                'label' => 'Telefone',
+            ),
+            'attributes' => array(
+                'class' => 'span3',
+            )
+        ));
+
+
+        // Celular
+        $this->add(array(
+            'name' => 'celular',
+            'type' => 'Zend\Form\Element\Text',
+            'options' => array(
+                'label' => 'Celular',
+            ),
+            'attributes' => array(
+                'class' => 'span3',
+            )
+        ));
+
+
+        // CEP
+        $this->add(array(
+            'name' => 'cep',
+            'type' => 'Zend\Form\Element\Text',
+            'options' => array(
+                'label' => 'CEP',
+            ),
+            'attributes' => array(
+                'class' => 'span3',
+            )
+        ));
+
+
+        // Endereco
+        $this->add(array(
+            'name' => 'endereco',
+            'type' => 'Zend\Form\Element\Text',
+            'options' => array(
+                'label' => 'Endereço',
+            ),
+            'attributes' => array(
+                'class' => 'span8',
+            )
+        ));
+
+
+        // Numero
+        $this->add(array(
+            'name' => 'numero',
+            'type' => 'Zend\Form\Element\Text',
+            'options' => array(
+                'label' => 'Número',
+            ),
+            'attributes' => array(
+                'class' => 'span2',
+            )
+        ));
+
+
+        // Complemento
+        $this->add(array(
+            'name' => 'complemento',
+            'type' => 'Zend\Form\Element\Text',
+            'options' => array(
+                'label' => 'Complemento',
+            ),
+            'attributes' => array(
+                'class' => 'span4',
+            )
+        ));
+
+
+        // Cidade
+        $this->add(array(
+            'name' => 'cidade',
+            'type' => 'Zend\Form\Element\Text',
+            'options' => array(
+                'label' => 'Cidade',
+            ),
+            'attributes' => array(
+                'class' => 'span4',
+            )
+        ));
+
+
+        // Bairro
+        $this->add(array(
+            'name' => 'bairro',
+            'type' => 'Zend\Form\Element\Text',
+            'options' => array(
+                'label' => 'Bairro',
+            ),
+            'attributes' => array(
+                'class' => 'span4',
+            )
+        ));
+
+
+        // UF
+        $this->add(array(
+            'name' => 'estado',
+            'type' => 'Zend\Form\Element\Select',
+            'options' => array(
+                'label' => 'Estado',
+                'options' => $this->getOptionsForSelect($this->estados, 'nome')
+            ),
+            'attributes' => array(
+                'class' => 'custom-select span4',
+            )
+        ));
+
+
+        // Senha
+        $this->add(array(
+            'name' => 'senha',
+            'type' => 'Zend\Form\Element\Password',
+            'options' => array(
+                'label' => 'Senha',
+            ),
+            'attributes' => array(
+                'class' => 'span4',
+            )
+        ));
+
+
+        // Confirmar Senha
+        $this->add(array(
+            'name' => 'confirmar_senha',
+            'type' => 'Zend\Form\Element\Password',
+            'options' => array(
+                'label' => 'Confirmar Senha',
+            ),
+            'attributes' => array(
+                'class' => 'span4',
+            )
+        ));
+
+
+        // Deseja receber notificações
+        $this->add(array(
+            'name' => 'noticicacoes',
+            'type' => 'Zend\Form\Element\Checkbox',
+            'options' => array(
+                'label' => 'Deseja receber notificações dos anúncios em sua região?',
+            ),
+        ));
+
+
+        // Button Cadastrar
+        $this->add(array(
+            'name' => 'cadastrar',
+            'type' => 'Zend\Form\Element\Submit',
+            'attributes' => array(
+                'value' => 'Cadastrar',
+                'class' => 'btn btn-large span2 btn-inverse pull-right',
+            )
+        ));
+	}
+
+
+
+    /** 
+     * getOptionsForSelect
+     *
+     * Empurramos os dados do $data no array para retorna-lo depois.
+     *
+     * @param array  $data
+     * @param string $column
+     *
+     * @return array $selectData
+     */
+    public function getOptionsForSelect($data, $column) {
+        $selectData = array();
+ 
+        foreach ($data as $value) {
+            $selectData[$value->id] = $value->$column;
+        }
+ 
+        return $selectData;
+    }
+}
---
+++ module/User/src/User/Form/CriarContaFilter.php
@@ -0,0 +1,209 @@
+<?php
+
+namespace User\Form;
+
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\Factory as InputFactory;
+use Zend\InputFilter\InputFilterInterface;
+use Zend\InputFilter\InputFilterAwareInterface;
+
+
+class CriarContaFilter implements InputFilterAwareInterface 
+{
+	protected $inputFilter;
+
+
+	public function setInputFilter(InputFilterInterface $inputFilter) {
+		throw new \Exception("Not used", 1);
+	}
+
+	public function getInputFilter() {
+		if (!$this->inputFilter) {
+			$inputFilter = new InputFilter();
+			$factory = new InputFactory();
+
+			// Nome Completo
+			$inputFilter->add(
+				$factory->createInput(
+					array(
+						'name' => 'nome',
+						'required' => true,
+						'filters'   => array(
+			                array('name' => 'StringTrim'),
+			            ),
+						'validators' => array( 
+							$this->notEmpty('Nome Completo')
+						),
+					)
+				)
+			);
+
+
+			// Data Nascimento
+			$inputFilter->add(
+				$factory->createInput(
+					array(
+						'name' => 'data_nasc',
+						'required' => true,
+						'validators' => array( 
+							$this->notEmpty('Data Nascimento')
+						),
+					)
+				)
+			);
+
+
+			// E-mail
+			$inputFilter->add(
+				$factory->createInput(
+					array(
+					    'name' => 'email',
+					    'required' => true,
+					    'filter' => array(
+					        'name' => 'StripTags',
+					    ),
+					    'validators' => array(
+					        array(
+					            'name' => 'NotEmpty',
+					            'break_chain_on_failure' => true,
+					            'options' => array(
+					                'messages' => array(
+					                    \Zend\Validator\NotEmpty::IS_EMPTY => 'O campo E-mail é de preenchimento obrigatório',
+					                ),
+					            ),
+					        ),
+					        array(
+					            'name' => 'EmailAddress',
+					            'options' => array(
+					            	'messages' => array(
+					                    \Zend\Validator\EmailAddress::INVALID_FORMAT => 'O E-mail digitado não é um endereço de e-mail válido. Favor preenche-lo corretamente.',
+					                ),
+					            ),
+					        ),
+					    ),
+					)
+				)
+			);
+
+
+			// CEP
+			$inputFilter->add(
+				$factory->createInput(
+					array(
+						'name' => 'cep',
+						'required' => true,
+						'validators' => array( 
+							$this->notEmpty('CEP')
+						),
+					)
+				)
+			);
+
+
+			// Endereco
+			$inputFilter->add(
+				$factory->createInput(
+					array(
+						'name' => 'endereco',
+						'required' => true,
+						'validators' => array( 
+							$this->notEmpty('Endereço')
+						),
+					)
+				)
+			);
+
+
+			// Número
+			$inputFilter->add(
+				$factory->createInput(
+					array(
+						'name' => 'numero',
+						'required' => true,
+						'validators' => array( 
+							$this->notEmpty('Número')
+						),
+					)
+				)
+			);
+
+
+			// Bairro
+			$inputFilter->add(
+				$factory->createInput(
+					array(
+						'name' => 'bairro',
+						'required' => true,
+						'validators' => array( 
+							$this->notEmpty('Bairro')
+						),
+					)
+				)
+			);
+
+
+			// Cidade
+			$inputFilter->add(
+				$factory->createInput(
+					array(
+						'name' => 'cidade',
+						'required' => true,
+						'validators' => array( 
+							$this->notEmpty('Cidade')
+						),
+					)
+				)
+			);
+
+
+			// Senha
+			$inputFilter->add(
+				$factory->createInput(
+					array(
+						'name' => 'senha',
+						'required' => true,
+						'validators' => array( 
+							$this->notEmpty('Senha')
+						),
+					)
+				)
+			);
+
+
+			// Confirmar senha
+			$inputFilter->add(
+				$factory->createInput(
+					array(
+						'name' => 'confirmar_senha',
+						'required' => true,
+						'validators' => array( 
+							$this->notEmpty('Confirmar senha')
+						),
+					)
+				)
+			);
+
+
+			$this->inputFilter = $inputFilter;
+		}
+
+		return $this->inputFilter;
+	}
+
+
+	/**
+     * notEmpty
+     *
+     * Valida se o campo está vazio
+     *
+     * @param  String $inputName
+     * @return \Zend\Validator\NotEmpty
+     */
+    protected function notEmpty($inputName)
+    {
+        $notEmpty = new \Zend\Validator\NotEmpty();
+        $notEmpty->setMessage("O campo {$inputName} é de preenchimento obrigatório", \Zend\Validator\NotEmpty::IS_EMPTY);
+
+        return $notEmpty;
+    }
+}
